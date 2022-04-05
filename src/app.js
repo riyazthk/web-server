4108a4f0e3be8cb0c63f4c7ce735a7d4efc1d8e0
TechnoTackle <technotackle@TechnoTackles-MacBook-Air.local>
2022-04-05 16:03:50 +0530

    create heroku serve

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const { foreCast } = require("./utils/forecast");
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 //Define paths for express config
 const publicPathDirectory = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views");
@@ -72,6 +74,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen("3000", () => {
+app.listen(port, () => {
   console.log("server is up");
 });
